faec3842ef0f7b079fbe38b2087b02ff54c630de
Lopes. <mlopesdiscord@gmail.com>
2025-10-02 07:21:15 -0300

    Add files via upload

---
+++ 3009/q1.js
@@ -0,0 +1,6 @@
+function numero(){
+    for(i=0; i> 100;i++){
+    document.getElementById("numero") = (i)
+}}
+
+numero()
---
+++ 3009/script.js
@@ -0,0 +1,18 @@
+function alterarMsg(nome){
+const elemento = document.getElementById("msg")
+elemento.innerHTML = `Boa tarde ${nome}!` 
+} 
+
+let elementoBotao = document.getElementById("btnConfirm")
+
+elementoBotao.addEventListener("click",  function(){
+    const numero = document.getElementById(numero)
+    for(i=0; i> 100;i++){
+    document.getElementById("numero") = `${i}`
+    }
+    const elemento = document.getElementById("msg")
+    const nome = document.getElementById("nome").value
+    console.log(nome)
+    elemento.innerHTML = `Boa tarde ${nome}!`
+    numero.innerHTML = `${numero}`
+})
